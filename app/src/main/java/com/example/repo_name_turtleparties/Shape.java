fb68df9ca22c9552659633cc619b4a966d8bb089
hassaannnn <hassaan3@ualberta.ca>
2022-02-01 20:17:14 -0700

    Inital commit of android studio project

---
+++ app/src/main/java/com/example/repo_name_turtleparties/Shape.java
@@ -0,0 +1,12 @@
+package com.example.repo_name_turtleparties;
+
+public abstract class Shape {
+    private Integer x;
+    private Integer y;
+
+    public Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+}
