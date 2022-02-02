ac9021495d258af8b6a7929b0989d0060cc8bb34
hassaan muhammad <98847820+hassaannnn@users.noreply.github.com>
2022-02-01 20:58:10 -0700

    fixing color

--- app/src/main/java/com/example/repo_name_turtleparties/Shape.java
+++ app/src/main/java/com/example/repo_name_turtleparties/Shape.java
@@ -4,7 +4,6 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public Shape(int x, int y){
         this.x = x;
