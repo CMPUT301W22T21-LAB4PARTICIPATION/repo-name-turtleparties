7ceb5987cd43877e069c8e252f973818fe2dde33
Gmaliakal <77520861+Gmaliakal@users.noreply.github.com>
2022-02-01 21:33:13 -0700

    purple

--- app/src/main/java/com/example/repo_name_turtleparties/Shape.java
+++ app/src/main/java/com/example/repo_name_turtleparties/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
 
-    String color = "yellow"
+    String color = "purple"
 
     public Shape(int x, int y){
         this.x = x;
