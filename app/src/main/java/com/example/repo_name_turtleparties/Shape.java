e16ca13e5fbdd9471dc1533e1fe9495fb452a079
Shane Smith <sasmith3@ualberta.ca>
2022-02-01 20:53:09 -0700

    add

--- app/src/main/java/com/example/repo_name_turtleparties/Shape.java
+++ app/src/main/java/com/example/repo_name_turtleparties/Shape.java
@@ -3,7 +3,7 @@ package com.example.repo_name_turtleparties;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "blue";
 
     public Shape(int x, int y){
         this.x = x;
