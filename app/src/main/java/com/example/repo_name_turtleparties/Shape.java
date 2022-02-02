519913da25874c995d1ba3a66429e7466f77486f
hassaannnn <hassaan3@ualberta.ca>
2022-02-01 20:36:37 -0700

    adding color string

--- app/src/main/java/com/example/repo_name_turtleparties/Shape.java
+++ app/src/main/java/com/example/repo_name_turtleparties/Shape.java
@@ -3,6 +3,7 @@ package com.example.repo_name_turtleparties;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+    String color = "blue";
 
     public Shape(int x, int y){
         this.x = x;
